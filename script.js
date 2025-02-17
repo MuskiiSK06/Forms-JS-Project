68b3d566a6dba539d6479a3ac7d5d1ba9ff1c646
MUSKAN SHAIKH <shaikhmuskii123@gmail.com>
2025-02-17 23:02:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function validate(){
+
+var studentname = document.getElementById('student-name');
+var gender = document.getElementById('gender');
+var email = document.getElementById('email');
+var classes = document.getElementById('classes-list');
+var phone = document.getElementById('phone');
+
+if (studentname.value ==""|| gender.value ==""|| email.value ==""|| classes.value ==""|| phone.value =="")
+    {
+    alert("Fill all the Blanks");
+   }
+
+   else{
+
+   }
+}
+
+
